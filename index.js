1401b113cbf47571bb3a53645c0d18e1ff262005
yuu0007 <69138047+yuu0007@users.noreply.github.com>
2020-09-23 23:02:27 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const fs = require('fs');
+const folders = fs.readdirSync('./commands/');
+const discordScript = require('discordbot-script');
+const bot = new discordScript({
+  token: process.env.BOT_TOKEN,
+  prefix: process.env.BOT_PREFIX
+});
+
+for (const files of folders) {
+  const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"));
+
+ for (const commands of folder) {
+    const command = require(`./commands/${files}/${commands}`);
+    bot.Command({
+      name: command.name,
+      aliases: command.aliases,
+      code: command.code
+    });
+  } 
+}
+
+bot.MessageEvent();
+
+
